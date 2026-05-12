9c49843c5cca6c4797c7385b8683f163665dacde
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2026-05-12 08:45:22 -0600

    feat(salestech): Implement the `estimate_estimates` stream directly from the available endpoint (#325)
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tap_service_titan/streams/sales_and_estimates.py
+++ tap_service_titan/streams/sales_and_estimates.py
@@ -2,15 +2,19 @@
 
 from __future__ import annotations
 
-from tap_service_titan.client import ServiceTitanExportStream
+from tap_service_titan.client import ServiceTitanExportStream, ServiceTitanStream
 from tap_service_titan.openapi_specs import SALESTECH, ServiceTitanSchema
 
 
-class _BaseSalesStream(ServiceTitanExportStream, api_prefix="/sales/v2"):
+class _BaseSalesExportStream(ServiceTitanExportStream, api_prefix="/sales/v2"):
     pass
 
 
-class EstimatesStream(_BaseSalesStream):
+class _BaseSalesStream(ServiceTitanStream, api_prefix="/sales/v2"):
+    pass
+
+
+class EstimatesStream(_BaseSalesExportStream):
     """Define estimates stream."""
 
     name = "estimates"
@@ -18,3 +22,16 @@ class EstimatesStream(_BaseSalesStream):
     primary_keys = ("id",)
     replication_key: str = "modifiedOn"
     schema = ServiceTitanSchema(SALESTECH, key="Estimates.V2.ExportEstimatesResponse")
+
+
+class EstimateItemsStream(_BaseSalesStream, active_any=True):
+    """Define estimate items stream.
+
+    https://developer.servicetitan.io/api-details/#api=tenant-salestech-v2&operation=Estimates_GetItems
+    """
+
+    name = "estimate_items"
+    path = "/estimates/items"
+    primary_keys = ("id",)
+    replication_key: str = "modifiedOn"
+    schema = ServiceTitanSchema(SALESTECH, key="Estimates.V2.EstimateItemResponse")
