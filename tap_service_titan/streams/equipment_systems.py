3e8abf61ed8915c45d52d09b3d1725c7aacdd0c4
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-07-08 10:20:38 -0600

    feat(equipment-systems,service-agreements): Implement the `equipment_systems` and `service_agreements_custom_fields` streams
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tap_service_titan/streams/equipment_systems.py
+++ tap_service_titan/streams/equipment_systems.py
@@ -2,12 +2,35 @@
 
 from __future__ import annotations
 
-from tap_service_titan.client import ServiceTitanStream
+from tap_service_titan.client import ServiceTitanExportStream, ServiceTitanStream
 from tap_service_titan.openapi_specs import EQUIPMENT_SYSTEMS, ServiceTitanSchema
 
 
 class _BaseEquipmentSystemsStream(ServiceTitanStream, api_prefix="/equipmentsystems/v2"):
     pass
+
+
+class _BaseEquipmentSystemsExportStream(
+    ServiceTitanExportStream,
+    api_prefix="/equipmentsystems/v2",
+):
+    pass
+
+
+class EquipmentSystemsStream(_BaseEquipmentSystemsExportStream):
+    """Define equipment systems stream.
+
+    https://developer.servicetitan.io/api-details/#api=tenant-equipment-systems-v2&operation=EquipmentSystems_GetExport
+    """
+
+    name = "equipment_systems"
+    path = "/export/equipment-systems"
+    primary_keys = ("id",)
+    replication_key: str = "modifiedOn"
+    schema = ServiceTitanSchema(
+        EQUIPMENT_SYSTEMS,
+        key="EquipmentSystems.V2.EquipmentSystemResponse",
+    )
 
 
 class InstalledEquipmentStream(_BaseEquipmentSystemsStream, active_any=True):
